93b235ce0e27a0ba7e328804d018dc7174f7bf52
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-12 11:32:49 +0200

    Add ex02

---
+++ ex02/ft_rev_params.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 09:35:01 by deydoux           #+#    #+#             */
+/*   Updated: 2023/07/12 11:32:21 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_swap(char **a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_rev_tab(char **tab, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = size - 1;
+	while (i < j)
+		ft_swap(&tab[i++], &tab[j--]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	ft_rev_tab(argv, argc);
+	i = 0;
+	while (i < argc - 1)
+	{
+		ft_putstr(argv[i++]);
+		write(1, "\n", 1);
+	}
+}
